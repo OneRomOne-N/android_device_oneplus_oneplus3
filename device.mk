c040fd0d5d7844a240574b4756f1d289965d9030
Sultanxda <sultanxda@gmail.com>
2017-07-15 02:39:06 -0700

    op3: Build weather providers
    These are no longer available in the Play Store.

--- device.mk
+++ device.mk
@@ -324,6 +324,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     sensors.msm8996
 
+# Weather providers
+PRODUCT_PACKAGES += \
+    OpenWeatherMapWeatherProvider \
+    YahooWeatherProvider \
+    WundergroundWeatherProvider
+
 # Wifi
 PRODUCT_PACKAGES += \
     ipacm \
