f8eb1dc1427c9f287a82a3e59523577ce7af6b68
joshuous <joshuous@gmail.com>
2017-08-03 14:48:01 +0000

    op3: Import preloaded-classes from OP5
    Signed-off-by: joshuous <joshuous@gmail.com>

--- device.mk
+++ device.mk
@@ -291,6 +291,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     power.msm8996
 
+# Preloaded classes
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/preloaded-classes:system/etc/preloaded-classes
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson
